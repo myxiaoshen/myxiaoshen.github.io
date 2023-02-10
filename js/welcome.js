aedd7698f82b867eb65d0546fb0addbb89fa4dd9
myxiaoshen <1097228498@qq.com>
2023-02-10 17:07:39 +0800

    Site updated: 2023-02-10 17:07:37

--- js/welcome.js
+++ js/welcome.js
@@ -1,10 +1,10 @@
 function welcome(){
 
-        welcome_text="小蓝盘目前出于维护状态开放时间待定~";//获取用户来源域名
+        welcome_text="小蓝盘目前出于维护状态开放时间待定~";
     swal({
-        title: "欢迎来到破站",
-        imageUrl: "/img/hi.png",//图片，自行修改位置
-        text: welcome_text="小蓝盘目前出于维护状态开放时间待定~",//欢迎文本，可自行修改
+        title: "欢迎！",
+        imageUrl: "/img/hi.png",
+        text: welcome_text="小蓝盘目前出于维护状态开放时间待定~", 
         timer: 2000,//弹出时间
         showConfirmButton: false
     });
